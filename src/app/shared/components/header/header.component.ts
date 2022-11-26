18cbdf765245ea4ad81d441169704b149cd91051
Manish Nayal <msnayal.55@gmail.com>
2022-11-26 20:43:10 +0530

    added admin dashboard

---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  @Output() toggleSidebarEvent: EventEmitter<any> = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  toggleSidebar() {
+    this.toggleSidebarEvent.emit();
+    setTimeout(() => {
+      window.dispatchEvent(
+        new Event('resize')
+      );
+    }, 300);
+  }
+
+}
